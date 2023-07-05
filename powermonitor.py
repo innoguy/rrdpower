54d432dcc4e6713b5c5ec19b8e099ed71087774d
Guy Coen <gcoen@cirrusled.com>
2023-07-05 21:55:51 +0800

    refactor

--- powermonitor.py
+++ powermonitor.py
@@ -1,84 +1,70 @@
-from signal import signal, SIGINT
-from sys import exit
-from os.path import exists
-from os import system 
-from datetime import datetime
-from time import sleep
-import json
-from dasbus.connection import SystemMessageBus
 from dasbus.loop import EventLoop
-import sys
+from dasbus.connection import SystemMessageBus
+from dasbus.identifier import DBusServiceIdentifier
+import json
+from os import system
+from time import sleep
+from threading import Thread
 
-DB="/var/log/cirrus-rrd/power"
-capture_window=3    # seconds to capture packet
-heartbeat=30        # seconds sleep between captures
-capture_space=60    # seconds between recorded values
-
-
-SPI1_Port = 'PortA'
-SPI1_Index = 1
-
-SPI2_Port = 'PortB'
-SPI2_Index = 1
+MESSAGE_BUS = SystemMessageBus()
+SERVICE = DBusServiceIdentifier(
+        namespace = ("com", "cirrusled", "Nucleus1"),
+        message_bus = MESSAGE_BUS
+)
 
 command = '''
 {"data":{"playerId":"00000000-0000-0000-0000-000000000000","topic":"CONTROLLER_COMMAND","payload":{"requestId":"123456789","portId":"","command":"REPORT_STATUS"}}}
 '''
 
-powerdata = {}
+SPI1_Port = 'Port A'
+SPI1_Index = '1'
+SPI2_Port = 'Port B'
+SPI2_Index = '1'
 
-def dump(loop, data):
-  global powerdata
-  try:
-    powerdata = json.dumps(json.loads(data), indent=2)
-  except:
-    print('Fail to parse the data:')
-    print(data)
-  loop.quit()
+DB="/var/log/cirrus-rrd/power"
 
-def sigint_handler(signal, frame):
-  exit(0)
-
-if __name__ == '__main__':
-  # handle the keyboard interrupt signal
-  signal.signal(signal.SIGINT, lambda x, y: sys.exit(0))
-  # start the d-bus test
-  loop = EventLoop()
-  bus = SystemMessageBus()
-  proxy = bus.get_proxy(
-    'com.cirrusled.Nucleus1',
-    '/com/cirrusled/Nucleus1')
-  while True:
-    proxy.StatusReportOccurred.connect(lambda data: dump(loop, data))
-    proxy.SendServerEvent(command)
-    loop.run()
+def callback(data):
+    SPI1_V = SPI1_I = SPI1_PF = SPI2_V = SPI2_I = SPI2_PF = 0
+    powerdata = json.loads(data)
     for port in powerdata["data"]["payload"]["ports"]:
-      if (port["id"]=='PortA'):
+      if (port["id"]=='Port A'):
         PortA_V = port["rmsVoltage"]
         PortA_I = port["rmsCurrent"]
         PortA_PF = port["powerFactor"]
-      elif (port["id"]=='PortB'):
+      elif (port["id"]=='Port B'):
         PortB_V = port["rmsVoltage"]
         PortB_I = port["rmsCurrent"]
         PortB_PF = port["powerFactor"]
-    
-    for port in powerdata["data"]["payload"]["spis"]:
-      if (port["portId"] == SPI1_Port and port["id"] == SPI1_Index):
-        SPI1_V  = port["rmsVoltage"]
-        SPI1_I  = port["rmsCurrent"]
-        SPI1_PF = port["powerFactor"]
-      elif (port["portId"] == SPI2_Port and port["id"] == SPI2_Index):
-        SPI2_V  = port["rmsVoltage"]
-        SPI2_I  = port["rmsCurrent"]
-        SPI2_PF = port["powerFactor"]
+    #if "spis" in powerdata["data"]["payload"]:
+    #  for port in powerdata["data"]["payload"]["spis"]:
+    #    if (port["portId"]==SPI1_Port and port["id"]==SPI1_Index):
+    #      SPI1_V = port["rmsVoltage"]
+    #      SPI1_I = port["rmsCurrent"]
+    #      SPI1_PF = port["powerFactor"]
+    #    elif (port["portId"] == SPI2_Port and port["id"] == SPI2_Index):
+    #      SPI2_V = port["rmsVoltage"]
+    #      SPI2_I = port["rmsCurrent"]
+    #      SPI2_PF = port["powerFactor"]
 
     shell = "rrdtool updatev {}.rrd N:{}:{}:{}:{}:{}:{}:{}:{}:{}:{}:{}:{}".format(
-              DB,
-              PortA_V, PortA_I, PortA_PF,
-              PortB_V, PortB_I, PortB_PF,
-              SPI1_V, SPI1_I, SPI1_PF,
-              SPI2_V, SPI2_I, SPI2_PF
-              )
+      DB,
+      PortA_V, PortA_I, PortA_PF,
+      PortB_V, PortB_I, PortB_PF,
+      SPI1_V, SPI1_I, SPI1_PF,
+      SPI2_V, SPI2_I, SPI2_PF)
     system(shell)
-    sleep(heartbeat)
+
+def trigger():
+  triggerproxy = SERVICE.get_proxy()
+  while True:
+    triggerproxy.SendServerEvent(command)
+    sleep(3)
+
+if __name__ == "__main__":
+    new_thread = Thread(target=trigger)
+    new_thread.start()
+    proxy = SERVICE.get_proxy()
+    proxy.StatusReportOccurred.connect(callback)
+    loop = EventLoop()
+    loop.run()
 
